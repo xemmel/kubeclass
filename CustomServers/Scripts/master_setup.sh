eb2a8218c5c7f9fc0f35d083260380b328ca7fb4
Morten la Cour <lacour@gmail.com>
2023-06-02 08:09:57 +0200

    master_setup

---
+++ CustomServers/Scripts/master_setup.sh
@@ -0,0 +1,10 @@
+sudo kubeadm init --kubernetes-version v1.26.5 --pod-network-cidr=192.168.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/custom-resources.yaml -O
+kubectl create -f custom-resources.yaml
+
