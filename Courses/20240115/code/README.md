bc8271c72f9ed26f565d4455fcaf0aabfcb508a7
Admin <Admin@TA-829-INS.kursdom.net>
2024-01-16 09:40:40 +0100

    class

---
+++ Courses/20240115/code/README.md
@@ -0,0 +1,16 @@
+### Docker build 
+
+In folder where *Dockerfile* resides
+
+```powershell
+
+docker build -t sleeper:0.1 .
+
+
+docker run -it -d -e x_delay=20000 sleeper:0.1
+
+
+docker logs 48b8ead3cdd20 -f
+
+
+```
---
+++ Courses/20240115/code/sleeper/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int sleep = 3000;
+
+string? sleepString = Environment.GetEnvironmentVariable("x_delay");
+if (!string.IsNullOrEmpty(sleepString))
+{
+    sleep = int.Parse(sleepString);
+}
+
+Console.WriteLine("Hello, World!");
+
+await Task.Delay(sleep);
+
+Console.WriteLine("Finished...Hello, World");
