6ff07fbc9a0185c154c5f76d5bd9caaf729585bd
Morten la Cour <lacour@gmail.com>
2024-05-08 16:24:49 +0200

    kind vms

--- Azure/create_kind_vms.ps1
+++ Azure/create_kind_vms.ps1
@@ -1,8 +1,33 @@
-## KUBE DISK
+### Start master VM
+
+
+az vm start --resource-group kubeclass-rg --name kubeclass1;
+
+### Shut down master VM
+
+az vm deallocate --resource-group kubeclass-rg --name kubeclass1 --no-wait
+
+### Create snapshot from Master VM
+
+$existingVMResourceId = "/subscriptions/9bc198aa-089c-4698-a7ef-8af058b48d90/resourceGroups/KUBECLASS-RG/providers/Microsoft.Compute/disks/kubeclass1_OsDisk_1_5bc00205a0874919b792bbe1fa747c75";
+
+
+$newSnapShotJson = az snapshot create `
+	--resource-group kubeclass-rg `
+	--name thekubesnapshot01 `
+	--location germanywestcentral `
+	--source $existingVMResourceId
+;
+
+$newSnapShot = $newSnapShotJson | ConvertFrom-Json;
+
+
+## New Kube student VM
 
 
 $snapshotId = "/subscriptions/9bc198aa-089c-4698-a7ef-8af058b48d90/resourceGroups/kubeclass-rg/providers/Microsoft.Compute/snapshots/thekubesnapshot"
 
+$snapshotId = $newSnapShot.id;
 
 
 
@@ -16,8 +41,8 @@ $diskSize = 127;
 $vmSize = "Standard_D4ds_v4";
 
 
-$rgName = "kubeclass-student4-rg";
-$vmName = "kubeclass-student4";
+$rgName = "kubeclass-student5-rg";
+$vmName = "kubeclass-student5";
 
 az group create --name $rgName --location $location;
 
