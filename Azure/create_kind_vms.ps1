4da89f7cbe95f2ec885773b77f09feffc0c4bf2f
Morten la Cour <lacour@gmail.com>
2024-06-21 19:05:39 +0200

    kind vms

--- Azure/create_kind_vms.ps1
+++ Azure/create_kind_vms.ps1
@@ -14,18 +14,49 @@ $existingVMResourceId = "/subscriptions/9bc198aa-089c-4698-a7ef-8af058b48d90/res
 
 $newSnapShotJson = az snapshot create `
 	--resource-group kubeclass-rg `
-	--name thekubesnapshot01 `
+	--name thekubesnapshot03 `
 	--location germanywestcentral `
+	--subscription 9bc198aa-089c-4698-a7ef-8af058b48d90 `
 	--source $existingVMResourceId
 ;
 
 $newSnapShot = $newSnapShotJson | ConvertFrom-Json;
 
+### Create a NSG Rule
+
+$myIp = (Invoke-WebRequest ifconfig.me/ip).Content.Trim();
+
+az network nsg rule create `
+	--name studentIps `
+	--nsg-name kubeclass1-nsg `
+	--resource-group kubeclass-rg `
+	--priority 1001 `
+	--access Allow `
+	--direction Inbound `
+	--protocol * `
+	--source-address-prefixes $myIp `
+	--source-port-ranges * `
+	--destination-port-ranges 3389 `
+	--subscription 9bc198aa-089c-4698-a7ef-8af058b48d90
+;
+
+### Remove NSG Rule
+
+az network nsg rule delete `
+	--name studentIps `
+	--nsg-name kubeclass1-nsg `
+	--resource-group kubeclass-rg `
+	--subscription 9bc198aa-089c-4698-a7ef-8af058b48d90
+;
+
+#### Start vm
+
+
 
 ## New Kube student VM
 
 
-$snapshotId = "/subscriptions/9bc198aa-089c-4698-a7ef-8af058b48d90/resourceGroups/kubeclass-rg/providers/Microsoft.Compute/snapshots/thekubesnapshot"
+$snapshotId = "/subscriptions/9bc198aa-089c-4698-a7ef-8af058b48d90/resourceGroups/kubeclass-rg/providers/Microsoft.Compute/snapshots/thekubesnapshot03"
 
 $snapshotId = $newSnapShot.id;
 
@@ -40,13 +71,16 @@ $diskSize = 127;
 
 $vmSize = "Standard_D4ds_v4";
 
+$studentNo = Read-Host("Student no");
+$rgName = "kubeclass-student${studentNo}-rg";
+$vmName = "kubeclass-student${studentNo}";
 
-$rgName = "kubeclass-student5-rg";
-$vmName = "kubeclass-student5";
+az group create --name $rgName --location $location --subscription 9bc198aa-089c-4698-a7ef-8af058b48d90;
 
-az group create --name $rgName --location $location;
-
-az disk create --resource-group $rgName --name $vmName --sku $diskSku --size-gb $diskSize --source $snapshotId
+az disk create --resource-group $rgName --name $vmName --sku $diskSku `
+	--size-gb $diskSize --source $snapshotId `
+	--subscription 9bc198aa-089c-4698-a7ef-8af058b48d90
+;
 
 
 az vm create `
@@ -56,5 +90,6 @@ az vm create `
 	--attach-os-disk $vmName `
 	--location $location `
 	--subnet $subnetId `
-	--size $vmSize
+	--size $vmSize `
+	--subscription 9bc198aa-089c-4698-a7ef-8af058b48d90
 ;
